b491162d1bf46c65315f488646bcc817850096b5
MonotoneChameleon <kyogo.sakai@gmail.com>
2023-09-29 18:25:15 +1000

    implemented new summary statistics

--- Main3.R
+++ Main3.R
@@ -2,9 +2,11 @@
 
 # Importing
 library(tidyverse)
+library(ggsignif)
+theme_set(theme_classic())
 
 # Sorting Files
-overview <- read.csv("../Overview4.csv")
+overview <- read.csv("../dataset2/Overview4.csv")
 
 # functions
 normalise <- function(n){
@@ -60,7 +62,7 @@ KS_test <- function(raw_data, number, label){
     ks1_2 <- ks.test(h1, h2, alternative = "two.sided")
     ks2_3 <- ks.test(h2, h3, alternative = "two.sided")
     ks2_avg <- ks.test(h2, (h1 + h3)/2, alternative = "two.sided")
-    stats <- data.frame(ks1_3 = ks1_3$p.value, ks1_2 = ks1_2$p.value, ks2_3 = ks2_3$p.value, ks2_avg = ks2_avg$p.value, location = number)
+    stats <- data.frame(ks1_3 = ks1_3$p.value, ks1_2 = ks1_2$p.value, ks2_3 = ks2_3$p.value, ks2_avg = ks2_avg$p.value, location = number, label = label)
     return(stats)
 }
 
@@ -79,7 +81,7 @@ subtraction_analysis <- function(raw_data, hist_data, j, eps, breaks, label){
         labs(title = paste(label), x = "Amplitude Histogram Difference, Normalised by Episodes", y = "during - AVG(before, after)")
     plot(fig1)
 
-    #mean <- data.frame()
+    #mean <- data.frame() 
 
     mean <- mean_from_hist(breaks[1:48], hdiff)
 
@@ -87,16 +89,29 @@ subtraction_analysis <- function(raw_data, hist_data, j, eps, breaks, label){
 }
 
 mean_from_hist <- function(bins, freqs){
+    #return(sum(bins * freqs)/sum(freqs))
+
+    # # Alternative summary statistic - if using mode instead of mean
+    # df <- data.frame(bins = bins, freqs = freqs)
+    # df <- arrange(df, freqs)
+    # return(df$bins[length(df$bins)])
+
+    # alternative mean if capping a max
+    bins <- bins[1:30]
+    freqs <- freqs[1:30]
+
     return(sum(bins * freqs)/sum(freqs))
+
 }
 
 # Execute commands
+KS_results_all <- data.frame()
 
 all_mean_amplitude <- data.frame()
 
 for (i in seq(1, length(overview$name))){
     # Dealing with particular cell:
-    filename <- paste('../', overview$name[i], sep = '')
+    filename <- paste('../dataset2/', overview$name[i], sep = '')
     celltype <- overview$cell_type[i]
     num_eps <- strsplit(overview$episodes[i], split = '%')
     distances <- strsplit(overview$distance[i], split = ' ')
@@ -125,6 +140,7 @@ for (i in seq(1, length(overview$name))){
         hist_data <- get_hist_data(raw_data, j, 49)
         plot_cum_hist(hist_data, j, cell_label, this_eps)
         KS_res <- KS_test(raw_data, j, cell_label)
+        KS_results <- rbind(KS_results, KS_res)
         hdiff <- subtraction_analysis(raw_data, hist_data, j, this_eps, 49, cell_label)
 
         dist_mean <- data.frame(dist = this_dist, mean = hdiff[[2]], location = j, celltype = celltype, file = filename)
@@ -133,12 +149,33 @@ for (i in seq(1, length(overview$name))){
         all_mean_amplitude <- rbind(all_mean_amplitude, dist_mean)
     }
     print(paste("finished", filename))
+    print(KS_results)
+    KS_results_all <- rbind(KS_results_all, KS_results)
 }
+
+    write.csv(KS_results_all, "KS_results_all.csv")
+
 
 a <- all_mean_amplitude %>%
     group_by(file) %>%
     arrange(dist) %>%
     summarise(furthest = mean[length(mean)], celltype = celltype[length(celltype)], dist = dist[length(dist)])
+
+# Fig 1a
+ggplot(a, aes(x = as.factor(celltype), y = furthest, fill = as.factor(celltype)), size = 2) + 
+    geom_bar(stat = "summary", fun.y = "mean", width = 0.5) + 
+    geom_jitter(color = '#00000088') + 
+    geom_errorbar(stat = 'summary', width = 0.2) + 
+    labs(x = "Cell Type", y = "Mean Amplitude (pA)") + 
+    scale_x_discrete(labels = c("SL", "SP")) + 
+    scale_fill_discrete(name = "Cell Type", labels = c("SL", "SP")) +
+    geom_signif(
+        annotation = formatC("*"),
+        y_position = 16, xmin = 1, xmax = 2,
+        tip_length = 0
+    )
+
+ggsave("SP_SL_bar.png", width = 4, height = 4, dpi = 300)
 
 a %>%
     group_by(celltype) %>%
@@ -160,6 +197,8 @@ ggplot(a, aes(x = as.factor(celltype), y = furthest, fill = as.factor(celltype))
 ggplot(all_mean_amplitude, aes(x = dist, y = mean, color = celltype)) + 
     geom_point() 
 
+ggsave("dist_amp_scatter.png", width = 4, height = 4, dpi = 300)
+
 test <- all_mean_amplitude %>% 
     group_by(celltype, file) %>%
     arrange(dist) %>%
@@ -169,8 +208,19 @@ test <- all_mean_amplitude %>%
 fig <- all_mean_amplitude %>% 
     group_by(celltype, file) %>%
     arrange(dist) %>%
-    mutate(norm_mean = mean) %>%
+    mutate(norm_mean = mean/mean[1]) %>%
     ggplot(aes(x = dist, y = norm_mean, color = celltype)) +
         geom_point(size = 2) + labs(title = "Mean amplitude by distance, normalised to first cell", x = "Distance (uM)", y = "Normalised mean mEPSC Amplitude (a.u.)")
 
 plot(fig)
+
+res <- t.test(a$furthest[a$celltype == "SL"], a$furthest[a$celltype == "SP"])
+res
+
+for (file in unique(all_mean_amplitude$file)){
+    data <- all_mean_amplitude[all_mean_amplitude$file == file, ] 
+    print(file)
+    plot <- ggplot(data, aes(x = dist, y = mean)) + geom_point(color = 'red', size = 5) + 
+        labs(x = "Distance (um)", y = "Mean mEPSC Amplitude (pA)", title = file)
+    plot(plot)
+}
